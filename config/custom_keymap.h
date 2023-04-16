ea59df3d293dfc43e82e3187994ca5d95ce1a4b8
Uku Raudvere <u.raudvere@gmail.com>
2023-04-16 20:02:54 +0300

    working state with estonian keys and braces

---
+++ config/custom_keymap.h
@@ -0,0 +1,19 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#include <dt-bindings/zmk/hid_usage.h>
+#include <dt-bindings/zmk/hid_usage_pages.h>
+#include <dt-bindings/zmk/modifiers.h>
+#include <dt-bindings/zmk/keys.h>
+
+//#include "keymap.h"
+// clang-format off
+
+// Aliases
+#define EE_1    N1    // 1
+#define UDIA RA(U)
+#define ADIA RA(A)
+#define OTIL RA(I)
+#define ODIA RA(O)
