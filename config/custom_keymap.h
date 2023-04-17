6d7de5422472202e4f02af39b0774cc127fa3a4e
Uku Raudvere <u.raudvere@gmail.com>
2023-04-17 10:59:48 +0300

    adjust colon/semicolon

--- config/custom_keymap.h
+++ config/custom_keymap.h
@@ -11,9 +11,25 @@
 //#include "keymap.h"
 // clang-format off
 
-// Aliases
-#define EE_1    N1    // 1
+// #define LC(keycode) APPLY_MODS(MOD_LCTL, keycode)
+// #define LS(keycode) APPLY_MODS(MOD_LSFT, keycode)
+// #define LA(keycode) APPLY_MODS(MOD_LALT, keycode)
+// #define LG(keycode) APPLY_MODS(MOD_LGUI, keycode)
+// #define RC(keycode) APPLY_MODS(MOD_RCTL, keycode)
+// #define RS(keycode) APPLY_MODS(MOD_RSFT, keycode)
+// #define RA(keycode) APPLY_MODS(MOD_RALT, keycode)
+// #define RG(keycode) APPLY_MODS(MOD_RGUI, keycode)
+
+//defined keys at https://github.com/zmkfirmware/zmk/blob/main/app/include/dt-bindings/zmk/keys.h
+
+
+// Aliases mapped to my custom xkb layout
 #define UDIA RA(U)
 #define ADIA RA(A)
 #define OTIL RA(I)
 #define ODIA RA(O)
+
+#define LESS RA(COMMA)
+#define GREATER RA(DOT)
+#define COLON LS(DOT)
+#define SEMI  LS(COMMA)
